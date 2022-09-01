166fd275c1e9dec1a221bbe984f97d2f778aa112
Sacdees <104862958+Sacdees@users.noreply.github.com>
2022-09-01 19:51:42 -0400

    fixes

--- bubble.js
+++ bubble.js
@@ -9,16 +9,32 @@ function buildCharts(sample) {
     Plotly.newPlot(); 
 
     // 1. Create the trace for the bubble chart.
-    var bubbleData = [
-   
-    ];
+    var bubbleData = [{
+      x: otuIds,
+      y: sampleValues,
+      text: otuLables,
+      mode: "markers",
+      marker: {
+        size: sampleValues,
+        color: otuIds,
+        colorscale: 'RdBu',
+      }
+      
+    }];
+  
 
     // 2. Create the layout for the bubble chart.
     var bubbleLayout = {
-      
+      title: "Bacteria Cultures per Sample",
+      xaxis: {title: "OTU ID"},
+      yaxis: {title: "Sample Value"},
+      titlefont: {"size": 25},
+      hovermode: "closest",
+      height: 500
     };
 
+
     // 3. Use Plotly to plot the data with the layout.
-    Plotly.newPlot(); 
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout);
   });
 }
