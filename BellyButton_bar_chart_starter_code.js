8b8b51c665f6a9d720aabc4c93751b1743acb361
Sacdees <104862958+Sacdees@users.noreply.github.com>
2022-09-01 20:24:49 -0400

    charts

--- BellyButton_bar_chart_starter_code.js
+++ BellyButton_bar_chart_starter_code.js
@@ -55,56 +55,33 @@ function buildMetadata(sample) {
 
 // 1. Create the buildCharts function.
 function buildCharts(sample) {
-  
   // 2. Use d3.json to load and retrieve the samples.json file 
-  
   d3.json("samples.json").then((data) => {
-    
     // 3. Create a variable that holds the samples array. 
-    
-    var samples = data.samples;
-    
+
     // 4. Create a variable that filters the samples for the object with the desired sample number.
-    
-    var numberSample = samples.filter(sampleObj => sampleObj.id == sample)
-    console.log(numberSample);
 
     //  5. Create a variable that holds the first sample in the array.
-    
-    var firstSample = numberSample[0];
-    console.log(firstSample);
+
 
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
-    
+
+
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
 
-    var yticks
+    var yticks = 
 
     // 8. Create the trace for the bar chart. 
-    
-    var barData = [{
-      x: sampleValuesSliced,
-      y: otuIdsSliced,
-      text: otuLablesSliced,
-      type: "bar",
-      orientation: 'h',
-      marker: {
-        color: 'MediumAquamarine',
-        opacity: 0.8,}
-    }];
-    
+    var barData = [
+      
+    ];
     // 9. Create the layout for the bar chart. 
-    var barLayout = {  
-      title: "Top 10 Bacteria Cultures Found",
-      titlefont: {"size": 25},
-      xaxis: {title: "Sample Value"}
+    var barLayout = {
      
     };
     // 10. Use Plotly to plot the data with the layout. 
-    Plotly.newPlot("bar", barData, barLayout);
+    
   });
 }
-
-//-----------------------
